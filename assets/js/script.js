39f2742705a7bd1abd0fa917338b8eba06a6efe6
durganp1 <durganp1@gmail.com>
2020-07-21 10:42:49 -0500

    MVP

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var cityEl = document.querySelector("#city-input");
 var inputButtonEl = document.querySelector("#button-addon2");
 var todayWeatherWrapperEl = document.querySelector("#weather-wrapper");
 var weatherForecastEl = document.querySelector("#forecast-container");
+var saveCityHistoryEl = document.querySelector("#city-history");
+var cityName = "";
 var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0');
@@ -14,8 +16,28 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+var saveCityHistory = function(city) {
+    
+    var cityHistory = document.createElement("li");
+        cityHistory.classList.add("todays-weather-data");
+        cityHistory.textContent = city;
+        saveCityHistoryEl.appendChild(cityHistory);
+        cityEl.value = "";
+};
+
+var savedCitySearch = function(event) {
+    debugger;
+    cityName = event.target.textContent;
+    cityNameSearch(cityName);
+    
+};
+
+saveCityHistoryEl.addEventListener("click", savedCitySearch);
+
+
 
 var cityNameSearch = function(city) {
+    
      var currentWeatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=188f221ff04011df267287eed52e6cf0";
      fetch(currentWeatherApi)
          .then(function(response) {
@@ -33,6 +55,7 @@ var cityNameSearch = function(city) {
         .catch(function(error) {
             alert("Unable to connect to Open Weather");
         });
+        
 };
 
 var cityForecast = function(data,lat, lon) {
@@ -54,13 +77,16 @@ var cityForecast = function(data,lat, lon) {
 };
 
 var citySubmitHandler = function(event) {
+    
     event.preventDefault();
-    var cityName = cityEl.value.trim();
+    
+        cityName = cityEl.value.trim();
     if (cityName) {
         cityNameSearch(cityName);
+        saveCitys(cityName);
         //selectCityEl(cityName);
         //console.log(cityName);
-        //cityEl.value = "";
+        
     }
     else {
         alert("Please enter a city name.");
@@ -70,9 +96,9 @@ var citySubmitHandler = function(event) {
 cityInputEl.addEventListener("submit", citySubmitHandler);
 
 var displayWeatherEl = function(data) {
-    //debugger;
+    weatherForecastEl.innerHTML = "";
     var cityDate = document.getElementById("city-date");
-        cityDate.textContent = capitalizeFirstLetter(cityEl.value.trim()) + " (" + today + ")";
+        cityDate.textContent = capitalizeFirstLetter(cityName + " (" + today + ")");
     var iconCode = data.current.weather[0].icon;
     var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
         $("#wicon").attr("src", iconUrl);
@@ -85,7 +111,13 @@ var displayWeatherEl = function(data) {
     var todaysUv = document.getElementById("todays-uv");
     var uvi = document.getElementById("uvi");
         uvi.textContent = data.current.uvi;
-        uvi.style.backgroundColor = "red";
+        if (uvi.textContent < 3) {
+            uvi.style.backgroundColor = "green";
+        } else if (uvi.textContent < 7){
+            uvi.style.backgroundColor = "orange";
+        } else {
+            uvi.style.backgroundColor = "red";
+        }
         todaysUv.textContent = "UV Index " + " - ";
     var forecastEl = document.getElementById("forecast-title");
         forecastEl.classList.add("forecast-title");
@@ -122,12 +154,16 @@ var displayWeatherEl = function(data) {
                 forecastDate.appendChild(forecastTemp);
             
                 weatherForecastEl.appendChild(forecastDate);
-            }; 
+            };  
+            saveCityHistory(cityName);
             
-        
-    
-    
-    
-    
 };
 
+var saveCitys = function(cityName) {
+
+    localStorage.setItem("city", cityName);
+};
+
+var loadCitys = function() {
+    cities = localStorage.getItem("city");
+};
